8c222c8515c67d57a7fddfb647928fda11a3e795
levi hicks <lv.hicks@gmail.com>
2022-03-13 22:48:54 -0500

    update tests

--- __tests__/blackjack.tsx
+++ __tests__/blackjack.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor } from '../utils/test-utils'
+import { render } from '../utils/test-utils'
 import userEvent from '@testing-library/user-event'
 import Blackjack from '../pages/blackjack'
 
@@ -7,30 +7,5 @@ test('deal button functionality', () => {
     const dealButton = getByRole('button', {name: /deal/i})
     userEvent.click(dealButton)
     const cards = getAllByTestId('playing-card')
-    expect(cards).toHaveLength(4)
+    expect(cards.length).toBeGreaterThanOrEqual(4)
 })
-
-test('reward functionality', async () => {
-    const { getByRole, getByText } = render(<Blackjack />)
-    const BET_AMOUNT = 1
-    const bankroll = getByText(/bankroll:/i)
-    const bankrollValue = Number(bankroll.textContent!.split('$')[1])
-    const dealButton = getByRole('button', { name: /deal/i })
-    userEvent.click(dealButton)
-    const standButton = getByRole('button', { name: /stand/i })
-    userEvent.click(standButton)
-    const winnerText = getByText(/wins/i)
-    const winner = winnerText.textContent!.split(' ')[0]
-    const newBankrollValue = Number(bankroll.textContent!.split('$')[1])
-    let winnings = BET_AMOUNT
-    if (winner === 'Player') 
-      winnings = BET_AMOUNT
-    else if (winner === 'Dealer') 
-      winnings = -BET_AMOUNT
-    else if (winner === 'Push.')
-      winnings = 0
-    else if (winner === 'Blackjack!')
-      winnings = 1.5 * BET_AMOUNT
-    waitFor(() => expect(newBankrollValue).toBe(bankrollValue + winnings))
-})
-
